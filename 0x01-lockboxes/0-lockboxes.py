d90df23809db255d6f96374eb0ed51ec5969a985
H. Sabah <sabahhasbi00@gmail.com>
2024-01-09 09:58:04 +0000

    Code fix: docstring for the win, again..

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+""" module for the functions canUnlockAll and open_sesame"""
+
+
 def canUnlockAll(boxes):
+    """Check if it's possible to unlock all boxes in a given list of boxes"""
     opened = [0]
     open_sesame(boxes, boxes[0], 0, opened)
     if len(boxes) == len(opened):
@@ -8,6 +12,7 @@ def canUnlockAll(boxes):
 
 
 def open_sesame(boxes, box, idx, opened):
+    """Recursive helper function to explore the connections between boxes"""
     if idx not in opened:
         opened.append(idx)
     for key in box:
