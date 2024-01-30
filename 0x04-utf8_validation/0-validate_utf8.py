54569294248f8bd875b1557e09f7cfd501520df9
H. Sabah <sabahhasbi00@gmail.com>
2024-01-30 08:08:39 +0000

    Code fix: pycodestyle and type annotation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """module for the function validUTF8"""
 
+from typing import List
 
-def validUTF8(data):
-    """A method that determines if a given data set represents a valid UTF-8 encoding."""
+
+def validUTF8(data: List[int]) -> bool:
+    """Returns True if a given data set represents a valid UTF-8 encoding."""
     if len(data) == 1:
         if data[0] <= 128:
             return True
