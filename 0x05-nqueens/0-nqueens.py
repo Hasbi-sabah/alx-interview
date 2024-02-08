17a1653059ab79e47eb4dc8e446474ad900afa87
H. Sabah <sabahhasbi00@gmail.com>
2024-02-08 16:09:34 +0000

    Update: refractored

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,6 +6,32 @@ on an NxN chessboard. Write a program that solves the N queens problem.
 
 import sys
 
+
+def is_available(queens, i, j):
+    """checks if a spot is available"""
+    for x, y in queens:
+        if i == x or j == y or abs(i - x) == abs(j - y):
+            return False
+    return True
+
+
+def queen_down(N, queens, final_combo):
+    """recursive function to put down non attacking queens"""
+    if len(queens) == N:
+        sorted_queens = sorted(queens, key=lambda x: x[0])
+        if sorted_queens not in final_combo:
+            final_combo.append(sorted_queens)
+        return final_combo
+    for i in range(N):
+        if any(i == x[0] for x in queens):
+            continue
+        for j in range(N):
+            if is_available(queens, i, j):
+                queen_down(N, queens + [[i, j]], final_combo)
+                break
+    return final_combo
+
+
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
     sys.exit(1)
@@ -19,44 +45,7 @@ except ValueError:
 if N < 4:
     print("N must be at least 4")
     sys.exit(1)
-
-board = []
-for i in range(N):
-    row = []
-    for y in range(N):
-        row.append(0)
-    board.append(row)
 queens = []
-
-
-def available_spots(queens, board):
-    """Check and mark available spots on a chessboard."""
-    for i, row in enumerate(board):
-        for j, square in enumerate(row):
-            for x, y in queens:
-                if i == x or j == y or abs(i - x) == abs(j - y):
-                    board[i][j] = 1
-
-
-def queen_down(N, queens, board, final_combo):
-    """
-    Place queens on a chessboard of size N,
-    exploring all combinations and returning unique solutions.
-    """
-    if len(queens) == N:
-        sorted_queens = sorted(queens, key=lambda x: x[0])
-        if sorted_queens not in final_combo:
-            final_combo.append(queens)
-        return final_combo
-    available_spots(queens, board)
-    for i, row in enumerate(board):
-        for j, square in enumerate(row):
-            if not square:
-                board_cpy = [r[:] for r in board]
-                queen_down(N, queens + [[i, j]], board_cpy, final_combo)
-    return final_combo
-
-
-queens = queen_down(N, queens, board, [])
+queen_down(N, [], queens)
 for queen in queens:
     print(queen)
